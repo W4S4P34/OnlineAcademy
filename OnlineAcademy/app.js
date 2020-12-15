38a8c9ffd7b6d4462706507d299f5e18e0159324
Samuel La <lathanhthai1905@gmail.com>
2020-12-15 23:50:35 +0700

    View: "Modified"; Controller: "None"
    Views:
    -----------------
    Reconstruct directory for views 
    
    Add partials for header and footer (handlebars)
    
    Main Layout: layouts/main.hbs
    - Created
    - Updated:
        + Use Handlebars `mustache` {{}} to dynamically render website
        + Add navigation bar (including collapse mode)
        + Add footer
    
    View1: vwHome/index.hbs
    - Created
    - Updated:
        + Add header - Carousel vfx
        + Add content - lorem ipsum
    
    View2: vwAbout/index.hbs
    - Created
    
    View3: vwCourses/index.hbs
    - Created
    
    View4: vwLogin/index.hbs
    - Created
    
    Controllers:
    -----------------
    None
    
    Externals:
    -----------------
    - Using template for views from WebThemez
    - Install external modules ~> static (Depending on WebThemez's template)
        + Bootstrap
        + Font Awesome
        + Camera
        + Isotope
        + Fancybox
        + Jquery
        + Modernizr
    
    Intermediate functions/helpers:
    -----------------
    None

--- OnlineAcademy/app.js
+++ OnlineAcademy/app.js
@@ -1,13 +1,15 @@
-// Framework support modules
+// Support framework modules
 const express = require('express');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
+
+const exphbs = require('express-handlebars');
 const ip = require("ip");
 
-//Self-Definition modules
+// Self-Definition modules
 const middleware = require('./middleware/middleware');
 
+// App-using declaration
 const app = express();
 require('dotenv').config();
 
@@ -15,23 +17,27 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(middleware.CheckAuthorized);
 
-
-
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
-    extname: '.hbs'
+    extname: '.hbs',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
 }));
+
+// Static declaration
+app.use('/views/assets', express.static('views/assets'));
+
+// Program configurations
 app.set('view engine', 'hbs');
+
 app.use(express.urlencoded({
     extended: true
 }));
 
-
-
+// Controllers declaration
 try {
     app.use('/', require('./controllers/course.route'));
     app.use('/user', require('./controllers/user.route'));
-
 } catch (e) {
     console.log(e);
 }
@@ -46,7 +52,7 @@ try {
 //    res.status(404).send("Link not found");
 //})
 
+// App start listening
 app.listen(process.env.PORT, ip.address(), function () {
     console.log(`App is listening at http://${ip.address()}:${process.env.PORT}`);
 })
-
