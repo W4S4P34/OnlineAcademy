43c5009741b18afef9e72b34ffe5b5b950578e36
HPQuyen <48430116+HPQuyen@users.noreply.github.com>
2020-12-21 00:05:31 +0700

    View: "Modified"; Controller: "Modified"
    Views:
    -----------------
    View1: Categories
    - apply database and display on view categories
    
    Controllers:
    -----------------
    Controller1: Course
    - Modified:
    + route: get courses by categories
    + model: find courses in database by categories
    Externals:
    -----------------
    None
    
    Intermediate funcs:
    -----------------
    None

--- OnlineAcademy/controllers/course.route.js
+++ OnlineAcademy/controllers/course.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     res.locals.currentView = '#home';
-    // await model.GetTopHighlightsCourse(2);
+    res.locals.listCourseFields = await model.GetAllFields();
     res.render('vwHome/index');
 })
 router.get('/about', (req, res) => {
@@ -20,6 +20,15 @@ router.get('/contact', (req, res) => {
     res.locals.currentView = '#contact';
     res.render('vwContact/index');
 })
-router.get('/course/web-development', (req, res) => {})
-router.get('/course/mobile-development', (req, res) => {})
+router.get('/course/byField/:field',async (req, res) => {
+    const result = await model.GetCourseByField(req.params.field);
+    console.log(req.params.field);
+    res.json(result);
+})
+router.get('/course/byTheme/:theme', async (req, res) => {
+    const result = await model.GetCourseByTheme(req.params.theme);
+    console.log(req.params.theme);
+    res.json(result);
+
+})
 module.exports = router;
--- OnlineAcademy/models/course.model.js
+++ OnlineAcademy/models/course.model.js
@@ -1,7 +1,8 @@
 const db = require('../utils/database');
+const e = require('express');
 
 module.exports = {
-    async GetAll() {
+    async GetAllCourses() {
         const sql = 'select * from course';
         const [rows, fields] = await db.load(sql);
         console.log(rows);
@@ -9,7 +10,7 @@ module.exports = {
             return null;
         return rows;
     },
-    async GetTopNewCourse(amount) {
+    async GetTopNewCourses(amount) {
         const sql = 'select * from course order by `date` desc limit ?';
         const condition = [amount];
         const [rows, fields] = await db.load(sql, condition);
@@ -18,7 +19,7 @@ module.exports = {
             return null;
         return rows;
     },
-    async GetTopHighlightsCourse(amount) {
+    async GetTopHighlightsCourses(amount) {
         const sql = 'select course.* from course join enroll on course.id = enroll.courseId group by course.title order by count(course.id) desc limit ?';
         const condition = [amount];
         const [rows, fields] = await db.load(sql, condition);
@@ -28,12 +29,54 @@ module.exports = {
         return rows;
     },
     async GetTopEnrollFields(amount) {
-        const sql = 'select course.title,count(course.id) from course join enroll on course.id = enroll.courseId group by course.title order by count(course.id) desc limit ?';
+        const sql = 'select f.name from course join enroll on course.id = enroll.courseId join fields as f on course.fieldsId = f.Id group by f.name order by count(course.id) desc limit ?';
         const condition = [amount];
         const [rows, fields] = await db.load(sql, condition);
         console.log(rows);
         if (rows.length == 0)
             return null;
         return rows;
-    }
+    },
+    async GetAllFields() {
+        const sql = 'select distinct name from fields';
+        const [rows, fields] = await db.load(sql);
+        if (rows.length == 0)
+            return null;
+        var listCourseFields = [];
+        for (var i = 0; i < rows.length; i++) {
+            const tmp = await this.GetThemeByField(rows[i].name);
+            listCourseFields.push({
+                name: rows[i].name,
+                empty: tmp == null,
+                listTheme: tmp
+            })
+            console.log(listCourseFields[i].listTheme);
+        }
+        console.log(listCourseFields);
+        return listCourseFields;
+    },
+    async GetThemeByField(name) {
+        const sql = 'select theme from fields where name = ?';
+        const condition = [name];
+        const [rows, fields] = await db.load(sql, condition);
+        if (rows.length == 0)
+            return null;
+        return rows;
+    },
+    async GetCourseByField(field) {
+        const sql = 'select course.* from fields join course on course.fieldsId = fields.Id and name = ?';
+        const condition = [field];
+        const [rows, fields] = await db.load(sql, condition);
+        if (rows.length == 0)
+            return null;
+        return rows;
+    },
+    async GetCourseByTheme(theme) {
+        const sql = 'select course.* from fields join course on course.fieldsId = fields.Id and fields.theme = ?';
+        const condition = [theme];
+        const [rows, fields] = await db.load(sql, condition);
+        if (rows.length == 0)
+            return null;
+        return rows;
+    },
 };
